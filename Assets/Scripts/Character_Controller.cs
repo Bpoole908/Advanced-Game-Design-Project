cd340fe27b55cc1acd3eb95bbe7ea96e9efd44ab
Bpoole908 <bpoole908@aol.com>
2018-01-29 08:59:40 -0500

    Added jumping and changed collider

--- Assets/Scripts/Character_Controller.cs
+++ Assets/Scripts/Character_Controller.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Character_Controller : MonoBehaviour {
 	public float speed = 10.0F;
+	public float jumpVelocity = 550.0f;
+	public float maxSlope = 60;
+	private bool grounded = false;
+	Rigidbody rb;
 	// Use this for initialization
 	void Start () {
 		//Turns off cursor on screen and locks inside game window
 		Cursor.lockState = CursorLockMode.Locked;
+		rb = gameObject.GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
@@ -21,7 +26,24 @@ public class Character_Controller : MonoBehaviour {
 
 		if (Input.GetKeyDown ("escape"))
 			Cursor.lockState = CursorLockMode.None;
-		 
 
+		if (Input.GetButtonDown ("Jump") && grounded) {
+			rb.AddForce (0, jumpVelocity, 0);
+		}
+
+	}
+	 void OnCollisionStay (Collision collision){
+		foreach (ContactPoint contact in collision.contacts){
+
+			if (Vector3.Angle(contact.normal, Vector3.up) < maxSlope){
+				print("true");
+				grounded = true;
+			}
+		}
+	}
+
+	void OnCollisionExit (Collision collision){
+		grounded = false;
+		print ("false");
 	}
 }
