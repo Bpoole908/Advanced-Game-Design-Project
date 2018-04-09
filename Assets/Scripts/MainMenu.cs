8b749c3e71a8369af39831c32a082505bca4d669
Unknown <vma@uncc.edu>
2018-04-08 21:24:52 -0400

    menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayLevelOne()
+	{
+		SceneManager.LoadScene ("Level_One");
+	}
+
+	public void PlayLevelTwo()
+	{
+		SceneManager.LoadScene ("Level_Two");
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log ("QUIT!");
+		Application.Quit ();
+	}
+}
