b445561432591d88295ca77e99864e7377a56248
Unknown <vma@uncc.edu>
2018-04-24 18:36:07 -0400

    Sounds and game title

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -10,18 +10,23 @@ public class Gun : MonoBehaviour {
 	public GameObject impactEffect;
 	[HideInInspector] public GameObject controller;
 	[HideInInspector] public GlobalVars target;
-
+    AudioSource gunSound;
+    AudioSource targetHit;
 
 	// Use this for initialization
 	void Start () {
 		controller = GameObject.Find ("GameController");
 		target = controller.GetComponent<GlobalVars> ();
+        AudioSource[] audios = GetComponents<AudioSource>();
+        gunSound = audios[0];
+        targetHit = audios[1];
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Fire1") && PauseSingle.Instance._enable) {
 			MuzzleFalsh.Play ();
+            gunSound.Play();
 			Shoot ();
 		}
 		
@@ -30,6 +35,7 @@ public class Gun : MonoBehaviour {
 		RaycastHit hit;
 		if (Physics.Raycast (fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
 			if (hit.transform.tag == "Target") {
+                targetHit.Play();
 				DestroyObject(hit.transform.gameObject);
 				target.getTargets = GameObject.FindGameObjectsWithTag ("Target");
 				target.targetCount = target.getTargets.Length -1;
